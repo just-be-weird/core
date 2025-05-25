05aaf4bf57e6dd0a35d7f9af84637a88a1f414ce
just-be-weird <17427511+just-be-weird@users.noreply.github.com>
2025-05-25 17:25:07 +0000

    Add theme lib

---
+++ libs/theme/src/lib/theme.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const StyledTheme = styled.div`
+  color: pink;
+`;
+
+export function Theme() {
+  return (
+    <StyledTheme>
+      <h1>Welcome to Theme!</h1>
+    </StyledTheme>
+  );
+}
+
+export default Theme;
